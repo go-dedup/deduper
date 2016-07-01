4ee51a61d623bf14ccf20aa9d96b1bd68d88757b
Tong Sun <suntong@cpan.org>
2016-07-01 10:48:23 -0400

    - [-] remove server

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"time"
 
 	"github.com/mauidude/deduper/minhash"
-	"github.com/mauidude/deduper/server"
-	"github.com/mauidude/deduper/server/command"
 )
 
 type config struct {
@@ -28,8 +26,6 @@ var cfg *config
 func init() {
 	cfg = &config{}
 
-	flag.StringVar(&cfg.host, "host", "localhost", "The HTTP host for this server to run on")
-	flag.IntVar(&cfg.port, "port", 8080, "The HTTP port for this server to run on")
 	flag.StringVar(&cfg.leader, "leader", "", "The HTTP host and port of the leader")
 	flag.BoolVar(&cfg.debug, "debug", false, "Enable debug logging")
 	flag.IntVar(&cfg.bands, "bands", 100, "Number of bands")
@@ -62,6 +58,5 @@ func main() {
 
 	mh := minhash.New(cfg.bands, cfg.rows, cfg.shingles)
 
-	s := server.New(path, cfg.host, cfg.port, mh)
 	log.Fatal(s.ListenAndServe(cfg.leader))
 }
