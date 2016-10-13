e6bd201440cf1f1dac6ee626e8f41f7d25a758b7
Tong Sun <suntong@cpan.org>
2016-10-13 16:44:18 -0400

    - [*] now deduper fully works

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/suntong/deduper/minhash"
+	"gopkg.in/suntong/deduper.v1/command"
+	"gopkg.in/suntong/deduper.v1/minhash"
 )
 
 type config struct {
@@ -19,6 +22,12 @@ type config struct {
 	bands    int
 	rows     int
 	shingles int
+	threshold float64
+}
+
+type testCase struct {
+	id    string
+	value string
 }
 
 var cfg *config
@@ -31,6 +40,7 @@ func init() {
 	flag.IntVar(&cfg.bands, "bands", 100, "Number of bands")
 	flag.IntVar(&cfg.rows, "hashes", 2, "Number of hashes to use")
 	flag.IntVar(&cfg.shingles, "shingles", 2, "Number of shingles")
+	flag.Float64Var(&cfg.threshold, "threshold", 0.5, "Threshold")
 }
 
 func main() {
@@ -45,18 +55,78 @@ func main() {
 	// Set the data directory.
 	if flag.NArg() == 0 {
 		flag.Usage()
-		log.Fatal("Data path argument required")
+		log.Fatal("Data string argument required")
 	}
 
-	path := flag.Arg(0)
-	if err := os.MkdirAll(path, 0744); err != nil {
-		log.Fatalf("Unable to create path: %v", err)
-	}
-
+	testStr := flag.Arg(0)
 	log.SetFlags(log.LstdFlags)
 
 	mh := minhash.New(cfg.bands, cfg.rows, cfg.shingles)
-	_ = mh
 
-	//log.Fatal(s.ListenAndServe(cfg.leader))
+	tests := []testCase{
+		{"p1", "hello world foo baz bar zomg"},
+		{"p2", "goodbye world foo qux bar zomg"},
+		{"p3", "entirely unrelated"},
+	}
+	for _, tt := range tests {
+		command.NewWriteCommand(tt.id, tt.value).Apply(mh)
+	}
+
+	matches := mh.FindSimilar(
+		strings.NewReader(testStr), cfg.threshold)
+
+	json.NewEncoder(os.Stdout).Encode(matches)
 }
+
+/*
+
+$ deduper
+Usage of deduper:
+  -bands int
+        Number of bands (default 100)
+  -debug
+        Enable debug logging
+  -hashes int
+        Number of hashes to use (default 2)
+  -leader string
+        The HTTP host and port of the leader
+  -shingles int
+        Number of shingles (default 2)
+  -threshold float
+        Threshold (default 0.5)
+Data string argument required
+
+$ deduper "hello world foo baz"
+[{"id":"p1","similarity":1}]
+
+$ deduper "world foo baz"
+[]
+
+$ deduper "unrelated"
+[]
+
+$ deduper "entirely unrelated"
+[{"id":"p3","similarity":1}]
+
+$ deduper "entire unrelate"
+[]
+
+$ deduper "unrelated" -threshold 0.00001
+[]
+
+$ deduper "foo qux bar zomg"
+[]
+
+$ deduper "foo qux bar zomg" -threshold 0.0000000001
+[]
+
+$ deduper "foo qux bar zomg world"
+[]
+
+$ deduper "foo qux bar zomg world goodbye"
+[]
+
+$ deduper "goodbye world foo qux bar zomg"
+[{"id":"p2","similarity":1}]
+
+*/
